3088918008a3f01f3d7e763c14870b5b0ff704e3
Piotr Szczesniak <piotr.j.szczesniak@gmail.com>
2011-12-16 23:50:17 +0100

    SNG: connection to CommServer added

--- src/Sensors/SNG/sngconnection.cpp
+++ src/Sensors/SNG/sngconnection.cpp
@@ -1,8 +1,16 @@
 #include "sngconnection.h"
 
-SNGconnection::SNGconnection(Configurator & config)
+SNGconnection::SNGconnection(Configurator & config) :
+    commServerHostName("localhost"), port(8800)
 {
     this->conf = &config;
+    commServer = new QTcpSocket(this);
+
+    connect(commServer, SIGNAL(readyRead()), this, SLOT(readFromSensor()));
+    connect(commServer, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(handleError(QAbstractSocket::SocketError)));
+
+    commServer->connectToHost(commServerHostName, port);
 }
 
 SNGconnection::~SNGconnection()
@@ -24,9 +32,19 @@ void SNGconnection::write(QVector<Message> messages)
     //it = messages.constBegin();
 }
 
-QVector<Message> SNGconnection::readAll() {
+QVector<Message> SNGconnection::readAll()
+{
     QVector<Message> res;
     return res;
+}
+
+void SNGconnection::readFromSensor()
+{
+}
+
+void SNGconnection::handleError(QAbstractSocket::SocketError error)
+{
+    qDebug() << "Error with CommServer: " << error;
 }
 
 
--- src/Sensors/SNG/sngconnection.h
+++ src/Sensors/SNG/sngconnection.h
@@ -2,6 +2,7 @@
 #define SNGCONNECTION_H
 
 #include "src/Sensors/devconnection.h"
+#include <QTcpSocket>
 
 class SNGconnection : DevConnection
 {
@@ -15,12 +16,20 @@ public:
     virtual void write(QVector<Message>);
     virtual QVector<Message> readAll();
 
+signals:
+    void readyToRead();
+
+public slots:
+    void readFromSensor();
+    void handleError(QAbstractSocket::SocketError);
+
 private:
     SNGconnection(Configurator&);
     Configurator* conf;
 
-signals:
-    void readyToRead();
+    QString commServerHostName ;
+    qint16 port;
+    QTcpSocket* commServer;
 };
 
 #endif // SNGCONNECTION_H
