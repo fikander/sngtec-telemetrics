6ab298b211b5bde23360f3dec48b69e82db1a347
Karol Polok <karol.w.polok@student.uw.edu.pl>
2011-12-18 19:10:07 +0100

    Added ModbusRtuFrame class

---
+++ src/Sensors/Modbus/modbusrtuframe.cpp
@@ -0,0 +1,49 @@
+#include "modbusrtuframe.h"
+
+#include <QByteArray>
+#include <QDebug>
+#include <string>
+
+
+ModbusRtuFrame::ModbusRtuFrame(unsigned char new_function, int new_data_length){
+    setAddr('1');
+    function = new_function;
+    data_length = new_data_length;
+    data = new unsigned char[data_length];
+    countCRC();
+}
+
+void ModbusRtuFrame::setAddr(unsigned char new_addr){
+    addr = new_addr;
+    countCRC();
+}
+
+void ModbusRtuFrame::setData(unsigned char* new_data, short new_data_length){
+    if (new_data_length == data_length)
+        memcpy(data, new_data, sizeof(data));
+    else
+        qDebug() << "Adding data error - wrong data size";
+    countCRC();
+}
+
+short int ModbusRtuFrame::showSize(){
+    return data_length;
+}
+
+void ModbusRtuFrame::countCRC(){ // To test
+    unsigned char* tmp = new unsigned char[data_length + 2];
+    tmp[0] = addr;
+    tmp[1] = function;
+    memcpy((tmp + 2), data, data_length);
+    crc = qChecksum((char*) tmp, data_length + 2);
+    delete []tmp;
+}
+
+unsigned char** ModbusRtuFrame::toSend(){
+    unsigned char** to_send = new unsigned char*[4];
+    to_send[0] = &addr;
+    to_send[1] = &function;
+    to_send[2] = data;
+    to_send[3] = (unsigned char*) &crc;
+    return to_send;
+}
---
+++ src/Sensors/Modbus/modbusrtuframe.h
@@ -0,0 +1,24 @@
+#ifndef MODBUSRTUFRAME_H
+#define MODBUSRTUFRAME_H
+
+
+class ModbusRtuFrame {
+
+    public: // add change function?
+        ModbusRtuFrame(unsigned char function, int new_data_length);
+        void setAddr(unsigned char new_addr);
+        void setData(unsigned char* new_data, short new_data_length);
+        void countCRC();
+        short int showSize();
+        unsigned char** toSend();
+
+    private:
+        unsigned char addr;
+        unsigned char function;
+        short data_length;
+        unsigned char* data;
+        unsigned short int crc;
+
+};
+
+#endif // MODBUSRTUFRAME_H
